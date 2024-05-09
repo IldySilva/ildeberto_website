d756548cc92811e1727d759a4e195c2972961e54
ildeberto <ildeberto34@gmail.com>
2024-05-09 01:50:47 +0100

    add sliver horizontal page

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
-
+import 'package:ildeberto_website/my_picture.dart';
+import 'dart:math' as math;
 import 'custom_scroll_behavior.dart';
 
 void main() {
@@ -16,12 +19,14 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  ScrollController _scrollController = ScrollController();
+  final ScrollController _scrollController = ScrollController();
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
+          textButtonTheme:
+              TextButtonThemeData(style: TextButton.styleFrom(textStyle: const TextStyle(color: Colors.white))),
           iconTheme: const IconThemeData(size: 20, color: Colors.white70),
           useMaterial3: true,
         ),
@@ -39,157 +44,146 @@ class _MyAppState extends State<MyApp> {
                   end: Alignment.bottomRight,
                 ))),
                 MouseRegion(
-                  cursor: SystemMouseCursors.grab,
-                  child: ScrollConfiguration(
-                    behavior: MyCustomScrollBehavior(),
-                    child: ListView(
-                      controller: _scrollController,
-                      scrollDirection: Axis.horizontal,
-                      padding: const EdgeInsets.only(left: 10, right: 10, top: 24, bottom: 24),
-                      children: [
-                        Stack(
+                    cursor: SystemMouseCursors.grab,
+                    child: ScrollConfiguration(
+                      behavior: MyCustomScrollBehavior(),
+                      child:
+                          CustomScrollView(controller: _scrollController, scrollDirection: Axis.horizontal, slivers: [
+                        SliverPersistentHeader(pinned: true, floating: false, delegate: SliverPicture()),
+
+                        SliverList.list(
                           children: [
                             Container(
-                              width: 320,
+                              width: 500,
                               decoration: const BoxDecoration(
-                                  image: DecorationImage(
-                                image: AssetImage("assets/images/picture.jpg"),
-                                fit: BoxFit.fitHeight,
-                              )),
+                                gradient: LinearGradient(
+                                  colors: [Color(0xff734f96), Color(0xffff9472)],
+                                  stops: [0, 1],
+                                  begin: Alignment.topLeft,
+                                  end: Alignment.bottomRight,
+                                ),
+                              ),
+                              child: Padding(
+                                padding: const EdgeInsets.all(20.0),
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    const SizedBox(
+                                      height: 30,
+                                    ),
+                                    Text(
+                                      "Hello,\nI'm Ildeberto",
+                                      textAlign: TextAlign.left,
+                                      style: GoogleFonts.poppins(
+                                          fontSize: 50, color: Colors.white, fontWeight: FontWeight.bold),
+                                    ),
+                                    const Divider(
+                                      endIndent: 200,
+                                    ),
+                                    const SizedBox(
+                                      height: 30,
+                                    ),
+                                    Text(
+                                      'A Mobile Software Developer, with more than 4 years of experience. I have a great learning ability, Currently Working with Flutter / Dart and .NET Core for full development',
+                                      textAlign: TextAlign.left,
+                                      style: GoogleFonts.poppins(
+                                          fontSize: 16, color: Colors.white, fontWeight: FontWeight.w300),
+                                    ),
+                                    const SizedBox(
+                                      height: 30,
+                                    ),
+                                    Row(
+                                      children: [
+                                        IconButton(
+                                            hoverColor: const Color(0xfff05053).withOpacity(0.6),
+                                            onPressed: () {},
+                                            icon: const Icon(
+                                              FontAwesomeIcons.github,
+                                            )),
+                                        IconButton(
+                                            hoverColor: const Color(0xfff05053).withOpacity(0.6),
+                                            onPressed: () {},
+                                            icon: const Icon(
+                                              FontAwesomeIcons.stackOverflow,
+                                            )),
+                                        IconButton(
+                                            hoverColor: const Color(0xfff05053).withOpacity(0.6),
+                                            onPressed: () {},
+                                            icon: const Icon(
+                                              FontAwesomeIcons.linkedin,
+                                            )),
+                                        IconButton(
+                                            hoverColor: const Color(0xfff05053).withOpacity(0.6),
+                                            onPressed: () {},
+                                            icon: const Icon(
+                                              FontAwesomeIcons.twitter,
+                                            )),
+                                        IconButton(
+                                            hoverColor: const Color(0xfff05053).withOpacity(0.6),
+                                            onPressed: () {},
+                                            icon: const Icon(
+                                              FontAwesomeIcons.medium,
+                                            )),
+                                        IconButton(
+                                            hoverColor: const Color(0xfff05053).withOpacity(0.6),
+                                            onPressed: () {},
+                                            icon: const Icon(
+                                              FontAwesomeIcons.youtube,
+                                            )),
+                                      ],
+                                    ),
+                                    const SizedBox(
+                                      height: 10,
+                                    ),
+                                    FloatingActionButton(
+                                      onPressed: () {
+                                        _scrollController.animateTo(_scrollController.offset + 500,
+                                            duration: const Duration(milliseconds: 1000), curve: Curves.easeIn);
+                                      },
+                                      child: const Icon(Icons.keyboard_arrow_right_sharp),
+                                    )
+                                  ],
+                                ),
+                              ),
                             ),
                             Container(
-                              width: 320,
-                              decoration: BoxDecoration(
-                                  gradient: LinearGradient(
-                                colors: [
-                                  const Color(0xffe1eec3).withOpacity(0.2),
-                                  const Color(0xfff05053).withOpacity(0.2)
-                                ],
-                                stops: const [0, 1],
-                                begin: Alignment.topLeft,
-                                end: Alignment.bottomRight,
-                              )),
+                              width: 500,
+                              decoration: const BoxDecoration(
+                                gradient: LinearGradient(
+                                  colors: [Color(0xffddd6f3), Color(0xfffaaca8)],
+                                  stops: [0, 1],
+                                  begin: Alignment.topLeft,
+                                  end: Alignment.bottomRight,
+                                ),
+                              ),
+                            ),
+                            Container(
+                              width: 500,
+                              decoration: const BoxDecoration(
+                                gradient: LinearGradient(
+                                  colors: [Color(0xfffeac5e), Color(0xffc779d0)],
+                                  stops: [
+                                    0,
+                                    01,
+                                  ],
+                                  begin: Alignment.topLeft,
+                                  end: Alignment.bottomRight,
+                                ),
+                              ),
+                            ),
+                            Container(
+                              width: 700,
+                              color: Colors.white,
                             ),
                           ],
                         ),
-                        Container(
-                          width: 500,
-                          decoration: const BoxDecoration(
-                              gradient: LinearGradient(
-                            colors: [Color(0xff1f1c2c), Color(0xff928dab)],
-                            stops: [0, 1],
-                            begin: Alignment.topLeft,
-                            end: Alignment.bottomRight,
-                          )),
-                          child: Padding(
-                            padding: const EdgeInsets.all(20.0),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                const SizedBox(
-                                  height: 30,
-                                ),
-                                Text(
-                                  "Hello,\nI'm Ildeberto",
-                                  textAlign: TextAlign.left,
-                                  style: GoogleFonts.poppins(
-                                      fontSize: 50, color: Colors.white, fontWeight: FontWeight.bold),
-                                ),
-                                const Divider(
-                                  endIndent: 200,
-                                ),
-                                const SizedBox(
-                                  height: 30,
-                                ),
-                                Text(
-                                  'A Mobile Software Developer, with more than 4 years of experience. I have a great learning ability, Currently Working with Flutter / Dart and .NET Core for full development',
-                                  textAlign: TextAlign.left,
-                                  style: GoogleFonts.poppins(
-                                      fontSize: 16, color: Colors.white, fontWeight: FontWeight.w300),
-                                ),
-                                const SizedBox(
-                                  height: 30,
-                                ),
-                                Row(
-                                  children: [
-                                    IconButton(
-                                        hoverColor: const Color(0xfff05053).withOpacity(0.6),
-                                        onPressed: () {},
-                                        icon: const Icon(
-                                          FontAwesomeIcons.github,
-                                        )),
-                                    IconButton(
-                                        hoverColor: const Color(0xfff05053).withOpacity(0.6),
-                                        onPressed: () {},
-                                        icon: const Icon(
-                                          FontAwesomeIcons.stackOverflow,
-                                        )),
-                                    IconButton(
-                                        hoverColor: const Color(0xfff05053).withOpacity(0.6),
-                                        onPressed: () {},
-                                        icon: const Icon(
-                                          FontAwesomeIcons.linkedin,
-                                        )),
-                                    IconButton(
-                                        hoverColor: const Color(0xfff05053).withOpacity(0.6),
-                                        onPressed: () {},
-                                        icon: const Icon(
-                                          FontAwesomeIcons.twitter,
-                                        )),
-                                    IconButton(
-                                        hoverColor: const Color(0xfff05053).withOpacity(0.6),
-                                        onPressed: () {},
-                                        icon: const Icon(
-                                          FontAwesomeIcons.medium,
-                                        )),
-                                    IconButton(
-                                        hoverColor: const Color(0xfff05053).withOpacity(0.6),
-                                        onPressed: () {},
-                                        icon: const Icon(
-                                          FontAwesomeIcons.youtube,
-                                        )),
-                                  ],
-                                ),
-                                const SizedBox(
-                                  height: 10,
-                                ),
-                                FloatingActionButton(
-                                  onPressed: () {
-                                    _scrollController.animateTo(_scrollController.offset + 500,
-                                        duration: const Duration(milliseconds: 1000), curve: Curves.easeIn);
-                                  },
-                                  child: const Icon(Icons.keyboard_arrow_right_sharp),
-                                )
-                              ],
-                            ),
-                          ),
-                        ),
-                        Container(
-                          width: 500,
-                          decoration: const BoxDecoration(
-                              gradient: LinearGradient(
-                            colors: [Color(0xff283048), Color(0xff859398)],
-                            stops: [0, 1],
-                            begin: Alignment.topLeft,
-                            end: Alignment.bottomRight,
-                          )),
-                        ),
-                        Container(
-                          width: 500,
-                          color: Colors.grey.shade200,
-                        ),
-                        Container(
-                          width: 500,
-                          color: Colors.grey,
-                        )
-                      ],
-                    ),
-                  ),
-                ),
+                      ]),
+                    ).animate().moveX().fade()),
               ],
             ),
           ),
         ));
   }
 }
+
---
+++ lib/my_picture.dart
@@ -0,0 +1,123 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class SliverPicture extends SliverPersistentHeaderDelegate {
+  @override
+  double get minExtent => 100;
+  @override
+  double get maxExtent => 320;
+
+  @override
+  Widget build(BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return AnimatedSwitcher(
+        duration: const Duration(milliseconds: 400),
+        child: shrinkOffset >= minExtent
+            ? FadeTransition(
+          key: const ValueKey<bool>(true),
+          opacity: const AlwaysStoppedAnimation<double>(1),
+          child: Stack(
+            children: [
+              Container(
+                width: 320,
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage("assets/images/picture.jpg"),
+                    fit: BoxFit.fitHeight,
+
+                  ),
+
+                ),
+
+              ),
+              ClipRRect(
+                child: BackdropFilter(
+                  filter: ImageFilter.blur(sigmaX: 2, sigmaY: 2),
+                  child: Container(
+                    decoration: const BoxDecoration(color: Colors.black87),
+                    width: 320,
+                  ),
+                ),
+              ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [ IconButton(
+              hoverColor: const Color(0xfff05053).withOpacity(0.6),
+              onPressed: () {},
+              icon: const Icon(
+                FontAwesomeIcons.github,
+              )),
+          IconButton(
+              hoverColor: const Color(0xfff05053).withOpacity(0.6),
+              onPressed: () {},
+              icon: const Icon(
+                FontAwesomeIcons.stackOverflow,
+              )),
+          IconButton(
+              hoverColor: const Color(0xfff05053).withOpacity(0.6),
+              onPressed: () {},
+              icon: const Icon(
+                FontAwesomeIcons.linkedin,
+              )),
+          IconButton(
+              hoverColor: const Color(0xfff05053).withOpacity(0.6),
+              onPressed: () {},
+              icon: const Icon(
+                FontAwesomeIcons.twitter,
+              )),
+          IconButton(
+              hoverColor: const Color(0xfff05053).withOpacity(0.6),
+              onPressed: () {},
+              icon: const Icon(
+                FontAwesomeIcons.medium,
+              )),
+          IconButton(
+              hoverColor: const Color(0xfff05053).withOpacity(0.6),
+              onPressed: () {},
+              icon: const Icon(
+                FontAwesomeIcons.youtube,
+              )),],),
+        )
+
+            ],
+          ),
+        )
+            : FadeTransition(
+          key: const ValueKey<bool>(false),
+          opacity: const AlwaysStoppedAnimation<double>(1),
+          child: Stack(
+            children: [
+              Container(
+                width: 320,
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage("assets/images/picture.jpg"),
+                    fit: BoxFit.fitHeight,
+                  ),
+                ),
+              ),
+              Container(
+                width: 320,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [const Color(0xffe1eec3).withOpacity(0.2), const Color(0xfff05053).withOpacity(0.2)],
+                    stops: const [0, 1],
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ));
+  }
+
+  @override
+  bool shouldRebuild(SliverPersistentHeaderDelegate oldDelegate) {
+    return false;
+  }
+}
